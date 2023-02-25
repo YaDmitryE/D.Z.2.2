0b9890938b40d33c6d1b766dacbaaac975e06746
YaDmitryE <120764954+YaDmitryE@users.noreply.github.com>
2023-02-25 13:21:48 +0300

    2

---
+++ 1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("A=");
+    scanf("%i", &a);
+    printf("B=");
+    scanf("%i", &b);
+    if (a > b) {
+        printf("A > B");
+    }
+    else {
+        if (a == b) {
+            printf("A = B");
+        }
+        else {
+            printf("A < B");
+        }
+    }
+    return 0;
+}
